d73dcfd60c56fdc01bff2c8f2fedcb6269201cee
Zach <zrp@pdx.edu>
2017-11-28 13:27:54 -0800

    various additions, changes, and structural changes

--- Server/irc_server.py
+++ Server/irc_server.py
@@ -1,15 +1,42 @@
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-import sys,socket,os,signal,shutil
+import sys,socket,os,signal,shutil,select
 
-# socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 host = ''  # could instead pass in '' in bind tuple
-port = int(sys.argv[1])  #LIMIT TO CERTAIN RANGE INSTEAD OF ACCEPTING INPUT?
-sock.bind((host, port))  # passing tuple as single argument
+port = 50000
+backlog = 5
+size = 1024
+
+# server/socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((host, port))  # passing tuple as single argument
 
 # accept call from client
-sock.listen(5)  # max one queued connection. Min 0
+server.listen(5)  # max one queued connection. Min 0
+reload(sys)
+sys.setdefaultencoding('UTF8')
+input = [server, sys.stdin]
+running = 1
+while running:
+    inputready,outputready,exceptready = select.select(input,[],[])
+    for s in inputready:
+        if s == server:
+            client, address = server.accept()
+            input.appent(client)
+        elif s == sys.stdin:
+            junk = sys.stdin.readline()
+            running = 0
+        else:
+            data = s.recv(size)
+            if data:
+                s.send(data)
+            else:
+                s.close()
+                input.remove(s)
+
+
 
 # initialization total
 IT = ''
@@ -20,12 +47,11 @@ csock = []
 # number of clients
 numc = 2
 
-reload(sys)
-sys.setdefaultencoding('UTF8')
+
 
 # accept from client
 for i in range(numc):
-    (client, ap) = sock.accept()
+    (client, ap) = server.accept()
     client.setblocking(0)  # non-blocking if set to 0
     csock.append(client)
 
@@ -42,7 +68,7 @@ while(len(csock) > 0):
         client.send(IT)
     except: pass
 
-sock.close()
+server.close()
 print 'Initialization Total is ', IT
 
 
@@ -59,3 +85,6 @@ def shutdown(self):
 '''def disconnect(clientList)
     for c in clientList
         if'''
+
+
+server.close()
